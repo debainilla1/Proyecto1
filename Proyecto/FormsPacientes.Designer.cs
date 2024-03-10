19e4a871e15d7705b134903740adebcd371e6b18
EliasGuerrero12 <jesusguerreropw12@gmail.com>
2024-03-10 12:43:30 -0600

    FormsPacientes

---
+++ Proyecto/FormsPacientes.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Proyecto
+{
+    partial class FrmPatient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LblName
+            // 
+            this.LblName.AutoSize = true;
+            this.LblName.BackColor = System.Drawing.SystemColors.Info;
+            this.LblName.Location = new System.Drawing.Point(12, 23);
+            this.LblName.Name = "LblName";
+            this.LblName.Size = new System.Drawing.Size(56, 16);
+            this.LblName.TabIndex = 0;
+            this.LblName.Text = "Nombre";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(75, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // FrmPatient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Proyecto.Properties.Resources.hospital_para_el_niño_poblano;
+            this.ClientSize = new System.Drawing.Size(969, 497);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LblName);
+            this.Name = "FrmPatient";
+            this.Text = "Pacientes";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblName;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
