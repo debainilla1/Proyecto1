3b6476ab8a705448ed4844b19cda399de9a3b687
Angel Eliseo Choperena Jimenez <angelchoj39@outlook.es>
2024-03-10 18:14:01 -0600

    MedicAndPatientCompanion

---
+++ Proyecto/Medic.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Proyecto
+{
+    partial class Medic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Medic));
+            this.LblName = new System.Windows.Forms.Label();
+            this.LblLastName = new System.Windows.Forms.Label();
+            this.LblProfessionalLicense = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtLastName = new System.Windows.Forms.TextBox();
+            this.TxtProfessionalLicense = new System.Windows.Forms.TextBox();
+            this.LblDoctorResponsibleForThePatient = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblName
+            // 
+            this.LblName.AutoSize = true;
+            this.LblName.BackColor = System.Drawing.Color.Transparent;
+            this.LblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblName.Location = new System.Drawing.Point(169, 196);
+            this.LblName.Name = "LblName";
+            this.LblName.Size = new System.Drawing.Size(87, 25);
+            this.LblName.TabIndex = 0;
+            this.LblName.Text = "Nombre";
+            // 
+            // LblLastName
+            // 
+            this.LblLastName.AutoSize = true;
+            this.LblLastName.BackColor = System.Drawing.Color.Transparent;
+            this.LblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblLastName.Location = new System.Drawing.Point(169, 268);
+            this.LblLastName.Name = "LblLastName";
+            this.LblLastName.Size = new System.Drawing.Size(90, 25);
+            this.LblLastName.TabIndex = 1;
+            this.LblLastName.Text = "Apellido";
+            // 
+            // LblProfessionalLicense
+            // 
+            this.LblProfessionalLicense.AutoSize = true;
+            this.LblProfessionalLicense.BackColor = System.Drawing.Color.Transparent;
+            this.LblProfessionalLicense.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblProfessionalLicense.Location = new System.Drawing.Point(169, 341);
+            this.LblProfessionalLicense.Name = "LblProfessionalLicense";
+            this.LblProfessionalLicense.Size = new System.Drawing.Size(204, 25);
+            this.LblProfessionalLicense.TabIndex = 2;
+            this.LblProfessionalLicense.Text = "Licencia profesional";
+            // 
+            // TxtName
+            // 
+            this.TxtName.BackColor = System.Drawing.SystemColors.Info;
+            this.TxtName.Location = new System.Drawing.Point(413, 199);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(323, 22);
+            this.TxtName.TabIndex = 3;
+            // 
+            // TxtLastName
+            // 
+            this.TxtLastName.BackColor = System.Drawing.SystemColors.Info;
+            this.TxtLastName.Location = new System.Drawing.Point(413, 268);
+            this.TxtLastName.Name = "TxtLastName";
+            this.TxtLastName.Size = new System.Drawing.Size(323, 22);
+            this.TxtLastName.TabIndex = 4;
+            this.TxtLastName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // TxtProfessionalLicense
+            // 
+            this.TxtProfessionalLicense.BackColor = System.Drawing.SystemColors.Info;
+            this.TxtProfessionalLicense.Location = new System.Drawing.Point(413, 344);
+            this.TxtProfessionalLicense.Name = "TxtProfessionalLicense";
+            this.TxtProfessionalLicense.Size = new System.Drawing.Size(323, 22);
+            this.TxtProfessionalLicense.TabIndex = 5;
+            // 
+            // LblDoctorResponsibleForThePatient
+            // 
+            this.LblDoctorResponsibleForThePatient.AutoSize = true;
+            this.LblDoctorResponsibleForThePatient.BackColor = System.Drawing.Color.Transparent;
+            this.LblDoctorResponsibleForThePatient.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDoctorResponsibleForThePatient.Location = new System.Drawing.Point(169, 93);
+            this.LblDoctorResponsibleForThePatient.Name = "LblDoctorResponsibleForThePatient";
+            this.LblDoctorResponsibleForThePatient.Size = new System.Drawing.Size(329, 25);
+            this.LblDoctorResponsibleForThePatient.TabIndex = 6;
+            this.LblDoctorResponsibleForThePatient.Text = "Medico responsable del paciente";
+            // 
+            // Medic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1033, 592);
+            this.Controls.Add(this.LblDoctorResponsibleForThePatient);
+            this.Controls.Add(this.TxtProfessionalLicense);
+            this.Controls.Add(this.TxtLastName);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.LblProfessionalLicense);
+            this.Controls.Add(this.LblLastName);
+            this.Controls.Add(this.LblName);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Medic";
+            this.Text = "Medic";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblName;
+        private System.Windows.Forms.Label LblLastName;
+        private System.Windows.Forms.Label LblProfessionalLicense;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtLastName;
+        private System.Windows.Forms.TextBox TxtProfessionalLicense;
+        private System.Windows.Forms.Label LblDoctorResponsibleForThePatient;
+    }
+}
---
+++ Proyecto/PatientCompanion.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace Proyecto
+{
+    partial class PatientCompanion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PatientCompanion));
+            this.LblName = new System.Windows.Forms.Label();
+            this.LblLastName = new System.Windows.Forms.Label();
+            this.LblPhoneNumber = new System.Windows.Forms.Label();
+            this.LblCellPhoneNumber = new System.Windows.Forms.Label();
+            this.LblEmail = new System.Windows.Forms.Label();
+            this.LblAge = new System.Windows.Forms.Label();
+            this.LblGender = new System.Windows.Forms.Label();
+            this.TxtName = new System.Windows.Forms.TextBox();
+            this.TxtLastName = new System.Windows.Forms.TextBox();
+            this.TxtCellPhoneNumber = new System.Windows.Forms.TextBox();
+            this.TxtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.TxtEmail = new System.Windows.Forms.TextBox();
+            this.TxtAge = new System.Windows.Forms.TextBox();
+            this.TxtGender = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LblName
+            // 
+            this.LblName.AutoSize = true;
+            this.LblName.BackColor = System.Drawing.Color.Transparent;
+            this.LblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblName.Location = new System.Drawing.Point(181, 117);
+            this.LblName.Name = "LblName";
+            this.LblName.Size = new System.Drawing.Size(79, 22);
+            this.LblName.TabIndex = 0;
+            this.LblName.Text = "Nombre";
+            // 
+            // LblLastName
+            // 
+            this.LblLastName.AutoSize = true;
+            this.LblLastName.BackColor = System.Drawing.Color.Transparent;
+            this.LblLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblLastName.Location = new System.Drawing.Point(181, 163);
+            this.LblLastName.Name = "LblLastName";
+            this.LblLastName.Size = new System.Drawing.Size(92, 22);
+            this.LblLastName.TabIndex = 1;
+            this.LblLastName.Text = "Apellidos";
+            // 
+            // LblPhoneNumber
+            // 
+            this.LblPhoneNumber.AutoSize = true;
+            this.LblPhoneNumber.BackColor = System.Drawing.Color.Transparent;
+            this.LblPhoneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblPhoneNumber.Location = new System.Drawing.Point(181, 210);
+            this.LblPhoneNumber.Name = "LblPhoneNumber";
+            this.LblPhoneNumber.Size = new System.Drawing.Size(172, 22);
+            this.LblPhoneNumber.TabIndex = 2;
+            this.LblPhoneNumber.Text = "Numero telefonico";
+            // 
+            // LblCellPhoneNumber
+            // 
+            this.LblCellPhoneNumber.AutoSize = true;
+            this.LblCellPhoneNumber.BackColor = System.Drawing.Color.Transparent;
+            this.LblCellPhoneNumber.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblCellPhoneNumber.Location = new System.Drawing.Point(181, 260);
+            this.LblCellPhoneNumber.Name = "LblCellPhoneNumber";
+            this.LblCellPhoneNumber.Size = new System.Drawing.Size(173, 22);
+            this.LblCellPhoneNumber.TabIndex = 3;
+            this.LblCellPhoneNumber.Text = "Numero de celular";
+            this.LblCellPhoneNumber.Click += new System.EventHandler(this.LblCellPhoneNumber_Click);
+            // 
+            // LblEmail
+            // 
+            this.LblEmail.AutoSize = true;
+            this.LblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.LblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblEmail.Location = new System.Drawing.Point(181, 308);
+            this.LblEmail.Name = "LblEmail";
+            this.LblEmail.Size = new System.Drawing.Size(59, 22);
+            this.LblEmail.TabIndex = 4;
+            this.LblEmail.Text = "Email";
+            // 
+            // LblAge
+            // 
+            this.LblAge.AutoSize = true;
+            this.LblAge.BackColor = System.Drawing.Color.Transparent;
+            this.LblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblAge.Location = new System.Drawing.Point(181, 352);
+            this.LblAge.Name = "LblAge";
+            this.LblAge.Size = new System.Drawing.Size(56, 22);
+            this.LblAge.TabIndex = 5;
+            this.LblAge.Text = "Edad";
+            // 
+            // LblGender
+            // 
+            this.LblGender.AutoSize = true;
+            this.LblGender.BackColor = System.Drawing.Color.Transparent;
+            this.LblGender.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblGender.Location = new System.Drawing.Point(181, 396);
+            this.LblGender.Name = "LblGender";
+            this.LblGender.Size = new System.Drawing.Size(76, 22);
+            this.LblGender.TabIndex = 6;
+            this.LblGender.Text = "Genero";
+            // 
+            // TxtName
+            // 
+            this.TxtName.BackColor = System.Drawing.Color.LemonChiffon;
+            this.TxtName.Location = new System.Drawing.Point(385, 117);
+            this.TxtName.Name = "TxtName";
+            this.TxtName.Size = new System.Drawing.Size(357, 22);
+            this.TxtName.TabIndex = 7;
+            // 
+            // TxtLastName
+            // 
+            this.TxtLastName.BackColor = System.Drawing.Color.LemonChiffon;
+            this.TxtLastName.Location = new System.Drawing.Point(385, 163);
+            this.TxtLastName.Name = "TxtLastName";
+            this.TxtLastName.Size = new System.Drawing.Size(357, 22);
+            this.TxtLastName.TabIndex = 8;
+            // 
+            // TxtCellPhoneNumber
+            // 
+            this.TxtCellPhoneNumber.BackColor = System.Drawing.Color.LemonChiffon;
+            this.TxtCellPhoneNumber.Location = new System.Drawing.Point(386, 260);
+            this.TxtCellPhoneNumber.Name = "TxtCellPhoneNumber";
+            this.TxtCellPhoneNumber.Size = new System.Drawing.Size(357, 22);
+            this.TxtCellPhoneNumber.TabIndex = 9;
+            // 
+            // TxtPhoneNumber
+            // 
+            this.TxtPhoneNumber.BackColor = System.Drawing.Color.LemonChiffon;
+            this.TxtPhoneNumber.Location = new System.Drawing.Point(385, 210);
+            this.TxtPhoneNumber.Name = "TxtPhoneNumber";
+            this.TxtPhoneNumber.Size = new System.Drawing.Size(357, 22);
+            this.TxtPhoneNumber.TabIndex = 10;
+            // 
+            // TxtEmail
+            // 
+            this.TxtEmail.BackColor = System.Drawing.Color.LemonChiffon;
+            this.TxtEmail.Location = new System.Drawing.Point(386, 308);
+            this.TxtEmail.Name = "TxtEmail";
+            this.TxtEmail.Size = new System.Drawing.Size(357, 22);
+            this.TxtEmail.TabIndex = 11;
+            // 
+            // TxtAge
+            // 
+            this.TxtAge.BackColor = System.Drawing.Color.LemonChiffon;
+            this.TxtAge.Location = new System.Drawing.Point(386, 352);
+            this.TxtAge.Name = "TxtAge";
+            this.TxtAge.Size = new System.Drawing.Size(357, 22);
+            this.TxtAge.TabIndex = 12;
+            // 
+            // TxtGender
+            // 
+            this.TxtGender.BackColor = System.Drawing.Color.LemonChiffon;
+            this.TxtGender.Location = new System.Drawing.Point(386, 396);
+            this.TxtGender.Name = "TxtGender";
+            this.TxtGender.Size = new System.Drawing.Size(357, 22);
+            this.TxtGender.TabIndex = 13;
+            // 
+            // PatientCompanion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(985, 585);
+            this.Controls.Add(this.TxtGender);
+            this.Controls.Add(this.TxtAge);
+            this.Controls.Add(this.TxtEmail);
+            this.Controls.Add(this.TxtPhoneNumber);
+            this.Controls.Add(this.TxtCellPhoneNumber);
+            this.Controls.Add(this.TxtLastName);
+            this.Controls.Add(this.TxtName);
+            this.Controls.Add(this.LblGender);
+            this.Controls.Add(this.LblAge);
+            this.Controls.Add(this.LblEmail);
+            this.Controls.Add(this.LblCellPhoneNumber);
+            this.Controls.Add(this.LblPhoneNumber);
+            this.Controls.Add(this.LblLastName);
+            this.Controls.Add(this.LblName);
+            this.Name = "PatientCompanion";
+            this.Text = "PatientCompanion";
+            this.Load += new System.EventHandler(this.PatientCompanion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblName;
+        private System.Windows.Forms.Label LblLastName;
+        private System.Windows.Forms.Label LblPhoneNumber;
+        private System.Windows.Forms.Label LblCellPhoneNumber;
+        private System.Windows.Forms.Label LblEmail;
+        private System.Windows.Forms.Label LblAge;
+        private System.Windows.Forms.Label LblGender;
+        private System.Windows.Forms.TextBox TxtName;
+        private System.Windows.Forms.TextBox TxtLastName;
+        private System.Windows.Forms.TextBox TxtCellPhoneNumber;
+        private System.Windows.Forms.TextBox TxtPhoneNumber;
+        private System.Windows.Forms.TextBox TxtEmail;
+        private System.Windows.Forms.TextBox TxtAge;
+        private System.Windows.Forms.TextBox TxtGender;
+    }
+}
