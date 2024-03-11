e8c0d114318e204425406773b3fbe0de87182d2b
BiggerManager99 <joseluisresendizbutron@gmail.com>
2024-03-10 21:24:21 -0600

    MedicStay forms

---
+++ Proyecto/Medic stay.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace Proyecto
+{
+    partial class Medic_stay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblDateOfEntry = new System.Windows.Forms.Label();
+            this.LblDateOfDischarge = new System.Windows.Forms.Label();
+            this.LblMedicalRecord = new System.Windows.Forms.Label();
+            this.LblTypeOfService = new System.Windows.Forms.Label();
+            this.LblOrigin = new System.Windows.Forms.Label();
+            this.LblReasonForDischarge = new System.Windows.Forms.Label();
+            this.LblChildBearingAgeWomen = new System.Windows.Forms.Label();
+            this.LblConditionsTreated = new System.Windows.Forms.Label();
+            this.LblComorbidities = new System.Windows.Forms.Label();
+            this.LblExternalCause = new System.Windows.Forms.Label();
+            this.LlblHospitalAcquiredInfection = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.TxtMedicalRecord = new System.Windows.Forms.TextBox();
+            this.TxtTypeOfService = new System.Windows.Forms.TextBox();
+            this.TxtOrigin = new System.Windows.Forms.TextBox();
+            this.TxtReasonForDischarge = new System.Windows.Forms.TextBox();
+            this.TxtChildBearingAgeWomen = new System.Windows.Forms.TextBox();
+            this.TxtConditionsTreated = new System.Windows.Forms.TextBox();
+            this.TxtComorbidities = new System.Windows.Forms.TextBox();
+            this.TxtExternalCause = new System.Windows.Forms.TextBox();
+            this.TxtHospitalAcquiredInfection = new System.Windows.Forms.TextBox();
+            this.LblMedicStay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LblDateOfEntry
+            // 
+            this.LblDateOfEntry.AutoSize = true;
+            this.LblDateOfEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDateOfEntry.Location = new System.Drawing.Point(15, 20);
+            this.LblDateOfEntry.Name = "LblDateOfEntry";
+            this.LblDateOfEntry.Size = new System.Drawing.Size(128, 16);
+            this.LblDateOfEntry.TabIndex = 0;
+            this.LblDateOfEntry.Text = "Fecha de ingreso";
+            // 
+            // LblDateOfDischarge
+            // 
+            this.LblDateOfDischarge.AutoSize = true;
+            this.LblDateOfDischarge.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblDateOfDischarge.Location = new System.Drawing.Point(16, 58);
+            this.LblDateOfDischarge.Name = "LblDateOfDischarge";
+            this.LblDateOfDischarge.Size = new System.Drawing.Size(102, 16);
+            this.LblDateOfDischarge.TabIndex = 1;
+            this.LblDateOfDischarge.Text = "Fecha de alta";
+            // 
+            // LblMedicalRecord
+            // 
+            this.LblMedicalRecord.AutoSize = true;
+            this.LblMedicalRecord.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMedicalRecord.Location = new System.Drawing.Point(18, 99);
+            this.LblMedicalRecord.Name = "LblMedicalRecord";
+            this.LblMedicalRecord.Size = new System.Drawing.Size(120, 16);
+            this.LblMedicalRecord.TabIndex = 2;
+            this.LblMedicalRecord.Text = "Historial Medico";
+            // 
+            // LblTypeOfService
+            // 
+            this.LblTypeOfService.AutoSize = true;
+            this.LblTypeOfService.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblTypeOfService.Location = new System.Drawing.Point(15, 142);
+            this.LblTypeOfService.Name = "LblTypeOfService";
+            this.LblTypeOfService.Size = new System.Drawing.Size(120, 16);
+            this.LblTypeOfService.TabIndex = 3;
+            this.LblTypeOfService.Text = "Tipo de servicio";
+            // 
+            // LblOrigin
+            // 
+            this.LblOrigin.AutoSize = true;
+            this.LblOrigin.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblOrigin.Location = new System.Drawing.Point(18, 179);
+            this.LblOrigin.Name = "LblOrigin";
+            this.LblOrigin.Size = new System.Drawing.Size(95, 16);
+            this.LblOrigin.TabIndex = 4;
+            this.LblOrigin.Text = "Procedencia";
+            // 
+            // LblReasonForDischarge
+            // 
+            this.LblReasonForDischarge.AutoSize = true;
+            this.LblReasonForDischarge.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblReasonForDischarge.Location = new System.Drawing.Point(18, 219);
+            this.LblReasonForDischarge.Name = "LblReasonForDischarge";
+            this.LblReasonForDischarge.Size = new System.Drawing.Size(109, 16);
+            this.LblReasonForDischarge.TabIndex = 5;
+            this.LblReasonForDischarge.Text = "Motivo del alta";
+            // 
+            // LblChildBearingAgeWomen
+            // 
+            this.LblChildBearingAgeWomen.AutoSize = true;
+            this.LblChildBearingAgeWomen.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblChildBearingAgeWomen.Location = new System.Drawing.Point(18, 258);
+            this.LblChildBearingAgeWomen.Name = "LblChildBearingAgeWomen";
+            this.LblChildBearingAgeWomen.Size = new System.Drawing.Size(131, 20);
+            this.LblChildBearingAgeWomen.TabIndex = 6;
+            this.LblChildBearingAgeWomen.Text = "Mujer En Edad Fertil";
+            this.LblChildBearingAgeWomen.UseCompatibleTextRendering = true;
+            // 
+            // LblConditionsTreated
+            // 
+            this.LblConditionsTreated.AutoSize = true;
+            this.LblConditionsTreated.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblConditionsTreated.Location = new System.Drawing.Point(15, 298);
+            this.LblConditionsTreated.Name = "LblConditionsTreated";
+            this.LblConditionsTreated.Size = new System.Drawing.Size(145, 16);
+            this.LblConditionsTreated.TabIndex = 7;
+            this.LblConditionsTreated.Text = "Afecciones tratadas";
+            // 
+            // LblComorbidities
+            // 
+            this.LblComorbidities.AutoSize = true;
+            this.LblComorbidities.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblComorbidities.Location = new System.Drawing.Point(16, 338);
+            this.LblComorbidities.Name = "LblComorbidities";
+            this.LblComorbidities.Size = new System.Drawing.Size(117, 16);
+            this.LblComorbidities.TabIndex = 8;
+            this.LblComorbidities.Text = "Comorbilidades";
+            // 
+            // LblExternalCause
+            // 
+            this.LblExternalCause.AutoSize = true;
+            this.LblExternalCause.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblExternalCause.Location = new System.Drawing.Point(15, 374);
+            this.LblExternalCause.Name = "LblExternalCause";
+            this.LblExternalCause.Size = new System.Drawing.Size(114, 16);
+            this.LblExternalCause.TabIndex = 9;
+            this.LblExternalCause.Text = "Causas externa";
+            // 
+            // LlblHospitalAcquiredInfection
+            // 
+            this.LlblHospitalAcquiredInfection.AutoSize = true;
+            this.LlblHospitalAcquiredInfection.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LlblHospitalAcquiredInfection.Location = new System.Drawing.Point(12, 416);
+            this.LlblHospitalAcquiredInfection.Name = "LlblHospitalAcquiredInfection";
+            this.LlblHospitalAcquiredInfection.Size = new System.Drawing.Size(185, 16);
+            this.LlblHospitalAcquiredInfection.TabIndex = 10;
+            this.LlblHospitalAcquiredInfection.Text = "Infección intrahospitalaria";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(172, 20);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 11;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(172, 58);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker2.TabIndex = 12;
+            // 
+            // TxtMedicalRecord
+            // 
+            this.TxtMedicalRecord.Location = new System.Drawing.Point(172, 99);
+            this.TxtMedicalRecord.Name = "TxtMedicalRecord";
+            this.TxtMedicalRecord.Size = new System.Drawing.Size(778, 22);
+            this.TxtMedicalRecord.TabIndex = 13;
+            // 
+            // TxtTypeOfService
+            // 
+            this.TxtTypeOfService.Location = new System.Drawing.Point(172, 142);
+            this.TxtTypeOfService.Name = "TxtTypeOfService";
+            this.TxtTypeOfService.Size = new System.Drawing.Size(778, 22);
+            this.TxtTypeOfService.TabIndex = 14;
+            // 
+            // TxtOrigin
+            // 
+            this.TxtOrigin.Location = new System.Drawing.Point(172, 179);
+            this.TxtOrigin.Name = "TxtOrigin";
+            this.TxtOrigin.Size = new System.Drawing.Size(778, 22);
+            this.TxtOrigin.TabIndex = 15;
+            // 
+            // TxtReasonForDischarge
+            // 
+            this.TxtReasonForDischarge.Location = new System.Drawing.Point(172, 216);
+            this.TxtReasonForDischarge.Name = "TxtReasonForDischarge";
+            this.TxtReasonForDischarge.Size = new System.Drawing.Size(778, 22);
+            this.TxtReasonForDischarge.TabIndex = 16;
+            // 
+            // TxtChildBearingAgeWomen
+            // 
+            this.TxtChildBearingAgeWomen.Location = new System.Drawing.Point(172, 258);
+            this.TxtChildBearingAgeWomen.Name = "TxtChildBearingAgeWomen";
+            this.TxtChildBearingAgeWomen.Size = new System.Drawing.Size(778, 22);
+            this.TxtChildBearingAgeWomen.TabIndex = 17;
+            // 
+            // TxtConditionsTreated
+            // 
+            this.TxtConditionsTreated.Location = new System.Drawing.Point(172, 298);
+            this.TxtConditionsTreated.Name = "TxtConditionsTreated";
+            this.TxtConditionsTreated.Size = new System.Drawing.Size(778, 22);
+            this.TxtConditionsTreated.TabIndex = 18;
+            // 
+            // TxtComorbidities
+            // 
+            this.TxtComorbidities.Location = new System.Drawing.Point(172, 338);
+            this.TxtComorbidities.Name = "TxtComorbidities";
+            this.TxtComorbidities.Size = new System.Drawing.Size(778, 22);
+            this.TxtComorbidities.TabIndex = 19;
+            // 
+            // TxtExternalCause
+            // 
+            this.TxtExternalCause.Location = new System.Drawing.Point(172, 374);
+            this.TxtExternalCause.Name = "TxtExternalCause";
+            this.TxtExternalCause.Size = new System.Drawing.Size(778, 22);
+            this.TxtExternalCause.TabIndex = 20;
+            // 
+            // TxtHospitalAcquiredInfection
+            // 
+            this.TxtHospitalAcquiredInfection.Location = new System.Drawing.Point(203, 413);
+            this.TxtHospitalAcquiredInfection.Name = "TxtHospitalAcquiredInfection";
+            this.TxtHospitalAcquiredInfection.Size = new System.Drawing.Size(747, 22);
+            this.TxtHospitalAcquiredInfection.TabIndex = 21;
+            // 
+            // LblMedicStay
+            // 
+            this.LblMedicStay.AutoSize = true;
+            this.LblMedicStay.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LblMedicStay.Location = new System.Drawing.Point(621, 33);
+            this.LblMedicStay.Name = "LblMedicStay";
+            this.LblMedicStay.Size = new System.Drawing.Size(148, 20);
+            this.LblMedicStay.TabIndex = 22;
+            this.LblMedicStay.Text = "Estancia Médica";
+            // 
+            // Medic_stay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(988, 504);
+            this.Controls.Add(this.LblMedicStay);
+            this.Controls.Add(this.TxtHospitalAcquiredInfection);
+            this.Controls.Add(this.TxtExternalCause);
+            this.Controls.Add(this.TxtComorbidities);
+            this.Controls.Add(this.TxtConditionsTreated);
+            this.Controls.Add(this.TxtChildBearingAgeWomen);
+            this.Controls.Add(this.TxtReasonForDischarge);
+            this.Controls.Add(this.TxtOrigin);
+            this.Controls.Add(this.TxtTypeOfService);
+            this.Controls.Add(this.TxtMedicalRecord);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.LlblHospitalAcquiredInfection);
+            this.Controls.Add(this.LblExternalCause);
+            this.Controls.Add(this.LblComorbidities);
+            this.Controls.Add(this.LblConditionsTreated);
+            this.Controls.Add(this.LblChildBearingAgeWomen);
+            this.Controls.Add(this.LblReasonForDischarge);
+            this.Controls.Add(this.LblOrigin);
+            this.Controls.Add(this.LblTypeOfService);
+            this.Controls.Add(this.LblMedicalRecord);
+            this.Controls.Add(this.LblDateOfDischarge);
+            this.Controls.Add(this.LblDateOfEntry);
+            this.Name = "Medic_stay";
+            this.Text = "Medic_stay";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblDateOfEntry;
+        private System.Windows.Forms.Label LblDateOfDischarge;
+        private System.Windows.Forms.Label LblMedicalRecord;
+        private System.Windows.Forms.Label LblTypeOfService;
+        private System.Windows.Forms.Label LblOrigin;
+        private System.Windows.Forms.Label LblReasonForDischarge;
+        private System.Windows.Forms.Label LblChildBearingAgeWomen;
+        private System.Windows.Forms.Label LblConditionsTreated;
+        private System.Windows.Forms.Label LblComorbidities;
+        private System.Windows.Forms.Label LblExternalCause;
+        private System.Windows.Forms.Label LlblHospitalAcquiredInfection;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.TextBox TxtMedicalRecord;
+        private System.Windows.Forms.TextBox TxtTypeOfService;
+        private System.Windows.Forms.TextBox TxtOrigin;
+        private System.Windows.Forms.TextBox TxtReasonForDischarge;
+        private System.Windows.Forms.TextBox TxtChildBearingAgeWomen;
+        private System.Windows.Forms.TextBox TxtConditionsTreated;
+        private System.Windows.Forms.TextBox TxtComorbidities;
+        private System.Windows.Forms.TextBox TxtExternalCause;
+        private System.Windows.Forms.TextBox TxtHospitalAcquiredInfection;
+        private System.Windows.Forms.Label LblMedicStay;
+    }
+}
---
+++ Proyecto/Medic stay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto
+{
+    public partial class Medic_stay : Form
+    {
+        public Medic_stay()
+        {
+            InitializeComponent();
+        }
+    }
+}
