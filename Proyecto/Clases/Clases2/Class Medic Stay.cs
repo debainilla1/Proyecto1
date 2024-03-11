afdceadfdaae847240005e3d0513f01b3cd1b93d
BiggerManager99 <joseluisresendizbutron@gmail.com>
2024-03-11 10:14:43 -0600

    Class Medic stay

---
+++ Proyecto/Clases/Clases2/Class Medic Stay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto
+{
+    internal class Class_Medic_Stay
+    {
+        public int ChildBearingAgeWoman { get; set; }
+        public DateTime DateOfEntry { get; set; }
+        public DateTime DateOfDischarge { get; set; }
+        public string MedicalRecord { get; set; }
+        public string TypeOfService { get; set; }
+        public string Origin { get; set; }
+        public string ReasonForDischarge { get; set; }
+        public string ConditionsTreated { get; set; }
+        public string Comorbidities { get; set; }
+        public string ExternalCause { get; set; }
+        public string HospitalAcquiredInfection { get; set; }
+    }
+}
